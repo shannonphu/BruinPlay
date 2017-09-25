4b9f81e227e50fc3a55aa4a5e228e06bc3e4a77e
shannonphu <shannonphu@sbcglobal.net>
2017-09-25 12:55:54 -0700

    added resource file and parse, installed handlebars and made basic view

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var express = require('express');
 
 // Instantiate the "app" to start creating server endpoints
 var app = express();
+var fs = require('fs');
+var path = require('path');
+
+app.set('view engine', 'hbs');
 
 // Server listens to port 3000
 app.listen(3000, function () {
@@ -11,5 +15,24 @@ app.listen(3000, function () {
 
 // Root web app endpoint
 app.get('/', function (request, response) {
-	response.send('Hello, world!');
+	var musicResources = readResources();
+	response.render('home', {
+		songs: musicResources
+	});
 });
+
+
+// Helper methods for reading resource file
+// TODO: async reading causing undefined to be returned before fs.readFile
+//       fix with callback?
+var readResources = function() {
+	fs.readFile(path.join(__dirname, 'resources.json'), {encoding: 'utf-8'}, function(err,data){
+	    if (!err) {
+	        var musicResources = JSON.parse(data);
+	        console.log(musicResources);
+	        return musicResources;
+	    } else {
+	        console.log(err);
+	    }
+	});
+};
