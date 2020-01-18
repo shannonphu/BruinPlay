6b508775520ee247415cb7a1081b7cd67dd5cdf4
Timothy Gu <timothygu99@gmail.com>
2020-01-18 14:37:15 -0800

    Install Material UI

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
         <p>
           Edit <code>src/App.js</code> and save to reload. Rucha is poopoo
         </p>
